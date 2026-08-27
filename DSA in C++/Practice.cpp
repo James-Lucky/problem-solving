c17f540ad229d38e83936f50490f051bd966aaca
jameslucky007 <105838230+James-Lucky@users.noreply.github.com>
2026-08-27 22:36:46 +0530

    changes

--- DSA in C++/Practice.cpp
+++ DSA in C++/Practice.cpp
@@ -1,35 +1,8 @@
 #include <iostream>
-#include <bits/stdc++.h>
+using namespace std;
 
-int main()
-{
-
-    std::queue<int> myQueue;
-    myQueue.push(10);
-    myQueue.push(20);
-    myQueue.push(30);
-
-    std::cout << "Queue size: " << myQueue.size() << std::endl;
-    std::cout << "Front element: " << myQueue.front() << std::endl;
-    std::cout << "Back element: " << myQueue.back() << std::endl;
-
-    std::cout << "Removing elements: ";
-    while (!myQueue.empty())
-    {
-        std::cout << myQueue.front() << " ";
-        myQueue.pop();
-    }
-    std::cout << std::endl;
-
-    if (myQueue.empty())
-    {
-        std::cout << "The queue is now empty." << std::endl;
-    }
-
-    
-    
-
-
+int main() {
+  
 
     return 0;
 }
